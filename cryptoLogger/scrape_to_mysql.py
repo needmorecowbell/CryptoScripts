d5795decf91ba0d0969d5a051a38ab95c6aadd88
needmorecowbell <amusciano@gmail.com>
2017-07-15 21:57:34 -0500

    Added db file

--- cryptoLogger/scrape_to_mysql.py
+++ cryptoLogger/scrape_to_mysql.py
@@ -7,15 +7,15 @@ response = requests.get('https://api.coinmarketcap.com/v1/ticker/').json()
 currencies = ["BTC", "ETH", "LTC", "MIOTA", "DASH"]
 #values to log (must be in the same order as sql database)
 values = [
-    "name", "symbol", "price_usd", "rank", "percent_change_1h",
+    "symbol", "price_usd", "rank", "percent_change_1h",
     "percent_change_24h", "percent_change_7d"
 ]
 
-#conn = MySQLdb.connect(host= "localhost",
-#                  user="root",
-#                  passwd="",
-#                  db="crypto")
-#x = conn.cursor()
+conn = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="",
+                  db="crypto")
+x = conn.cursor()
 
 try:
     appendedVals = ""
@@ -26,11 +26,11 @@ try:
                     appendedVals += ("," + crypto[item])
                     print(item + ": " + crypto[item])
                 else:
-                    appendedVals += (",")
+                    appendedVals += (",")#Insert null value
             appendedVals = appendedVals[1:]  #cleanse  of the first ','
             print(
                 """INSERT INTO VALUES """ + appendedVals
-            )  #INSERT INTO VALUES (name),(symbol),(price_usd),(rank),(change1h),(change24h),(change7d)
+            )  #INSERT INTO VALUES (symbol),(price_usd),(rank),(change1h),(change24h),(change7d)
             appendedVals = ""
             #x.execute("""INSERT INTO  VALUES """+appendedVals)
             # conn.commit()
